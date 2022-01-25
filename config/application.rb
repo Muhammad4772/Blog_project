ca127f8eb522068e627c233bd43c8726b6e2f8f4
Muhammad4772 <mudasirbhatti62300@gmail.com>
2022-01-25 19:07:21 +0500

    add secret key

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module BlogProject
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.secret_key_base = "62299913c41b7a0506ea242ec5263b6d84c5096c575f37d620e0ac133211fc77b066100e52e50345be3fef2c9cb231c004aefcdf5f1507b9fb5332902aced08f"
   end
 end
